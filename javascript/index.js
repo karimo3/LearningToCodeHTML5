2b68cd16d98cb39e7e66c6e078943a0764bc77d7
Karim Obaid <kokarim@msn.com>
2018-01-10 13:12:14 -0500

    Jan102017

--- javascript/index.js
+++ javascript/index.js
@@ -1,3 +1,37 @@
+$(document).ready (function() {
+   
+   /*Positioning*/
+    var top = document.getElementById("topText");
+    var left = document.getElementById("leftText");
+    var pos = document.getElementById("positioning");
+    
+    document.getElementById("btnPosition").onclick = function () {
+    var img = document.getElementById("orange2");
+    img.style.position = pos.value;
+    img.style.left = left.value + "px";
+    img.style.top = top.value + "px";
+    }
+    /* Positioning...*/
+
+
+// var video;
+// function play() {
+// 	video.play();
+// }
+
+// function pause() {
+// 	video.pause();
+// }
+
+// function back() {
+// 	video.currentTime -= 10;
+//}
+
+
+
+
+}); //end of jQuery
+
 function changeImage() {
     var image = document.getElementById("myImage");
     if (image.src.match("bulbon")) {
@@ -13,29 +47,41 @@ function loginerror(){
     });
 };
 
+function Red(evt) {
+    var circle = evt.target;
+    circle.setAttribute("style", "fill: red");
+    }
+    function Green(evt) {
+    var circle = evt.target;
+    circle.setAttribute("style", "fill: green");
+    }
+    
+    
+    function allowDrop(ev) {
+        ev.preventDefault();
+    }
+    
+    function drag(ev) {
+        ev.dataTransfer.setData("text", ev.target.id);
+    }
+    
+    function drop(ev) {
+        ev.preventDefault();
+        var data = ev.dataTransfer.getData("text");
+        ev.target.appendChild(document.getElementById(data));
+    }
 
-var video;
-function play() {
-	video.play();
-}
-
-function pause() {
-	video.pause();
-}
-
-function back() {
-	video.currentTime -= 10;
-}
-
-/* working on canvas element */
+    /* working on canvas element */
 var drawCanvas = document.getElementById("drawCanvas");
-var ctxt = drawCanvas.getContext("2d");
+var ctxt = null;
+ctxt = drawCanvas.getContext("2d");
 ctxt.beginPath();
 ctxt.moveTo(125,20)
 ctxt.bezierCurveTo(0,200,300,300,50,400);
 ctxt.lineWidth = 25;
 ctxt.strokeStyle = '#0f0';
 ctxt.stroke();
+
 
 var drawingSurface = document.getElementById("ctxt2");
 var ctxt2 = drawingSurface.getContext("2d");
@@ -50,13 +96,3 @@ ctxt2.fill();
 ctxt2.stroke();
 
 /*SVG element work */
-
-function Red(evt) {
-var circle = evt.target;
-circle.setAttribute("style", "fill: red");
-}
-function Green(evt) {
-var circle = evt.target;
-circle.setAttribute("style", "fill: green");
-}
-
